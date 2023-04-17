993a268fe18ea9c6480480eff8ebc1a53290803c
Jacksson Yuri <78384230+JackssonYuri@users.noreply.github.com>
2023-04-17 18:28:44 -0300

    Create double, triple and square root.py
    Desafio 006 - Crie um algoritmo que leia um número e mostre o seu dobro, triplo e raiz quadrada

---
+++ python course/double, triple and square root.py
@@ -0,0 +1,9 @@
+number = float(input())
+
+dobro = number * 2
+triplo = number * 3
+raiz_quadrada = number ** (1/2)
+
+print('Dobro = {}'.format(dobro))
+print('Triplo = {}'.format(triplo))
+print('Raiz quadrada = {:.3f}'.format(raiz_quadrada))
