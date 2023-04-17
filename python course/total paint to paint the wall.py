c84e98ee637a57060364070fcda13cbc53d84032
Jacksson Yuri <78384230+JackssonYuri@users.noreply.github.com>
2023-04-17 18:31:34 -0300

    Create total paint to paint the wall.py
    Desafio 011 - Faça um programa que leia a largura e a altura de uma parede em metros, calcule sua área e a quantidade de tinta necessária para pintá-la, sabendo que cada litro de tinta pinta uma área de 2m^2

---
+++ python course/total paint to paint the wall.py
@@ -0,0 +1,9 @@
+altura = float(input())
+largura = float(input())
+
+area = altura * largura
+
+tinta = area / 2
+
+print("Area {}".format(area))
+print("Tinta pra pintá-la {} litros".format(tinta))
