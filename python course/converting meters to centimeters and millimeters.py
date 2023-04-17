6f4be9d81a8187546341e4fb324956cee838bca0
Jacksson Yuri <78384230+JackssonYuri@users.noreply.github.com>
2023-04-17 18:29:51 -0300

    Create converting meters to centimeters and millimeters.py
    Desafio 008 - Escreva um programa que leia um valor em metros e o exiba convertido em centímetros e milímetros

---
+++ python course/converting meters to centimeters and millimeters.py
@@ -0,0 +1,5 @@
+valor = int(input())
+
+valor_centimetros = valor / 100
+valor_milimetros = valor_centimetros / 100
+print('{} metros\nem centímetros são: {} e em milímetros são: {}'.format(valor, valor_centimetros, valor_milimetros))
