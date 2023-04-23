53c1cebdcc6f7605cc03d2f1d589680927b571df
Jacksson Yuri <78384230+JackssonYuri@users.noreply.github.com>
2023-04-23 09:21:27 -0300

    Create comparing numbers.py
    DESAFIO 38 - Escreva um programa que leia dois números inteiros e compare-os, mostrando na tela uma mensagem:
    
    O primeiro valor é maior
    O segundo valor é maior
    Não existe valor maior, os dois são iguais.

---
+++ python course/comparing numbers.py
@@ -0,0 +1,9 @@
+a = int(input('Primeiro valor: '))
+b = int(input('Segundo valor: '))
+
+if a > b: 
+  print('O primeiro valor é maior')
+elif b > a: 
+  print('O segundo valor é maior')
+else:
+  print('Não existe valor maior, os dois são iguais')
